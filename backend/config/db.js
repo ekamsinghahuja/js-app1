8bb3d292efc6d2b8748a4dd1228df9a7e14d40f6
ekamsinghahuja <ekamsinghahuja123@gmail.com>
2024-06-03 18:43:12 +0530

    commit-2

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+
+
 export const connectDb = async ()=>{
-    await mongoose.connect('mongodb+srv://ekamsinghahuja123:ekamsinghahuja123@cluster0.zothmyw.mongodb.net/projects1').then(()=>console.log('db connected'))
+    await mongoose.connect(process.env.MONGO_URI).then(()=>console.log('db connected'))
 }
